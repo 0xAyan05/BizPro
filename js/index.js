f27dec534224037d75681f2fe8fdfee6eef1b5da
0xAyan05 <ianxrepomanta29@gmail.com>
2024-04-03 07:15:54 +0800

    major changes in whole code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function showRegistrationForm() {
+    document.getElementById("registration-form-container").style.display = "block";
+  }
+
+  function closeRegistrationForm() {
+    document.getElementById("registration-form-container").style.display = "none";
+  }
+
+  function closeLoginForm() {
+    document.getElementById("login-form-container").style.display = "none";
+  }
