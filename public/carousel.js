d66e119d071b7a01e86b8524e950a7485fd9c3e0
Manuel Caballero <07307477@dwc-legazpi.edu>
2024-04-03 15:28:56 +0800

    added carousel functionality

--- public/carousel.js
+++ public/carousel.js
@@ -1,4 +1,28 @@
 const carousel = document.querySelector('.carousel')
+const btn = document.querySelectorAll('.carousel-container button')
+let dragging = false, start, distance
+
+btn.forEach(b =>{
+    b.addEventListener('click', e => {
+        console.log(carousel.scrollLeft)
+        carousel.scrollLeft += b.className=="left"? -265: 265
+    })
+})
+
+carousel.addEventListener('mousedown', e => {
+    dragging = true
+    start = e.pageX
+    distance = carousel.scrollLeft
+})
+window.addEventListener('mouseup', e =>{
+    dragging = false
+})
+carousel.addEventListener('mousemove', e => {
+   if(!dragging) return
+   
+   else carousel.scrollLeft = distance - (e.pageX - start) *1.5
+})
+
 
 fetch('http://localhost:5000/api/services')
 .then(res => res.json())
@@ -6,10 +30,11 @@ fetch('http://localhost:5000/api/services')
     console.log(data)
     data.forEach(e => {
         carousel.innerHTML += `
-        <div class="card">
-        <p>${e.services}</p>
-        <img  src="${e.poster}" ></img>
+        <div class="card" style="background-image: url('${e.poster}')" >
+        <div class="text" >${e.services}</div>
         </div>`
     })
 })
 .catch(err => console.log(err))
+
+//
