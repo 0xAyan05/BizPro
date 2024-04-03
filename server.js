b7bf862688bf15940a85294066b08ebc7676a43b
Manuel Caballero <07307477@dwc-legazpi.edu>
2024-04-03 11:57:43 +0800

    added posters to api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const top_services = require('./utils/services.js')
 const PORT = process.env.PORT || 5000
 
 app.use(express.urlencoded({ extended: true }))
@@ -8,7 +9,12 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res)=>{
-    res.sendFile('index.html', { root: './public/views'})
+    res.status(200).sendFile('index.html', { root: './public/views'})
 })
+
+app.get('/api/services', (req, res)=>{
+    res.status(200).json(top_services)
+})
+
 
 app.listen(PORT)
