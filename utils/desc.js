40636355bd0fa3b59bef5ff956c1e32b2d6b7744
0xAyan05 <ianxrepomanta29@gmail.com>
2024-04-04 13:58:12 +0800

    added data on desc schema

--- utils/desc.js
+++ utils/desc.js
@@ -9,7 +9,7 @@ const desc = [
     },
     SubscriptionDetails: {
       Duration: "Self-paced",
-      Price: "$1500",
+      Price: "₱1500",
       Instructor: "Owen Nelson",
       VerifiedExpert: true,
       Access: "Unlimited",
@@ -43,7 +43,7 @@ const desc = [
     },
     SubscriptionDetails: {
       Duration: "Self-paced",
-      Price: "$1400",
+      Price: "₱1400",
       Instructor: "Sophia Chen",
       VerifiedExpert: true,
       Access: "Unlimited",
@@ -63,6 +63,126 @@ const desc = [
         Participant: "Michael R.",
         Review:
           "This course has been a game-changer for me. It's opened my eyes to the potential of AI in business strategy, and I'm excited to apply what I've learned to my organization. Five stars!",
+      },
+    ],
+    AverageRating: "5/5 stars",
+  },
+
+  {
+    Course: {
+      Title: "The Future of Work: AI and Automation",
+      Instructor: "Maxwell Johnson",
+      Category: "Technology",
+      Description:
+        "Discover the impact of AI and automation on the future of work and learn how to adapt and thrive in the changing landscape.",
+    },
+    SubscriptionDetails: {
+      Duration: "Self-paced",
+      Price: "₱1200",
+      Instructor: "Maxwell Johnson",
+      VerifiedExpert: false,
+      Access: "Unlimited",
+    },
+    Comments: [
+      {
+        Participant: "Adrian R.",
+        Review:
+          "Maxwell's course on AI and automation was insightful and helped me understand the future of work better.",
+      },
+      {
+        Participant: "Emerson O.",
+        Review:
+          "The course was well-structured and provided practical insights into AI and automation.",
+      },
+    ],
+    AverageRating: "4/5 stars",
+  },
+
+  {
+    Course: {
+      Title: "Ethical AI in Business",
+      Instructor: "Emily Wang",
+      Category: "Technology",
+      Description:
+        "Learn about the ethical implications of AI in business and develop strategies for responsible AI adoption and implementation.",
+    },
+    SubscriptionDetails: {
+      Duration: "Self-paced",
+      Price: "₱1500",
+      Instructor: "Emily Wang",
+      VerifiedExpert: true,
+      Access: "Unlimited",
+    },
+    Comments: [
+      {
+        Participant: "Manuel C.",
+        Review:
+          "Emily's course on Ethical AI in Business was enlightening. It provided a comprehensive understanding of ethical considerations in AI.",
+      },
+      {
+        Participant: "Mark B.",
+        Review:
+          "The course was insightful and helped me understand how to implement AI ethically in business.",
+      },
+    ],
+    AverageRating: "5/5 stars",
+  },
+
+  {
+    Course: {
+      Title: "AI-Powered Business Innovation",
+      Instructor: "Michael Li",
+      Category: "Technology",
+      Description:
+        "Discover how AI is driving business innovation and learn how to harness AI technologies to foster innovation in your organization.",
+    },
+    SubscriptionDetails: {
+      Duration: "Self-paced",
+      Price: "₱1100",
+      Instructor: "Michael Li",
+      VerifiedExpert: false,
+      Access: "Unlimited",
+    },
+    Comments: [
+      {
+        Participant: "Alex S.",
+        Review:
+          "Michael's course on AI-Powered Business Innovation was very insightful. It helped me understand how to leverage AI for innovation in my business.",
+      },
+      {
+        Participant: "Jane D.",
+        Review:
+          "The course was engaging and provided practical examples of AI in business innovation. Highly recommended",
+      },
+    ],
+    AverageRating: "4/5 stars",
+  },
+
+  {
+    Course: {
+      Title: "AI for Business Leaders",
+      Instructor: "Ava Patel",
+      Category: "Technology",
+      Description:
+        "Gain insights into AI technologies and their business applications to make informed decisions and drive business growth.",
+    },
+    SubscriptionDetails: {
+      Duration: "Self-paced",
+      Price: "₱1400",
+      Instructor: "Ava Patel",
+      VerifiedExpert: true,
+      Access: "Unlimited",
+    },
+    Comments: [
+      {
+        Participant: "John D.",
+        Review:
+          "Ava's course on AI for Business Leaders was incredibly insightful. It provided me with the knowledge I needed to understand AI's role in business.",
+      },
+      {
+        Participant: "Jane S.",
+        Review:
+          "The course was very informative and helped me see how AI can be leveraged for business growth. Highly recommended!",
       },
     ],
     AverageRating: "5/5 stars",
